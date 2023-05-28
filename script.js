f0cb679de96142ba6586c4037ec4a6340ea0a86d
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-28 16:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const strong = document.getElementsByTagName("strong");
 function highlight() {
     //Write your code here
-
-
+	for(let str of strong){
+		str.style.color = "green"
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
+	for(let str of strong){
+		str.style.color = "black"
+	}
     
 }
